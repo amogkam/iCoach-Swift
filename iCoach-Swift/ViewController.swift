99bd51ba7acfd25a8953f939a333ab89c069c826
Amog Kamsetty <amogkamsetty@gmail.com>
2015-10-31 12:32:28 -0700

    Initial Commit

---
+++ iCoach-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCoach-Swift
+//
+//  Created by Amog Kamsetty on 10/31/15.
+//  Copyright (c) 2015 Amog Kamsetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
